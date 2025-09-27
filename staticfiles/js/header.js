2721c04c84d2a2ecb88a954ccb3f7c6356ab895d
Henry Allison <hyallison5050@gmail.com>
2025-09-27 11:03:57 +0200

    header initial fix to be mobile friendly

---
+++ staticfiles/js/header.js
@@ -0,0 +1,5 @@
+// static/js/header.js
+
+// You can add your JavaScript code here.
+// For now, this is just a placeholder to resolve the 404 error.
+console.log("Header script loaded successfully.");
